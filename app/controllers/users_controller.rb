f0c331e9ba83f39d1a768a2c708edbc4e5090c24
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-06 14:42:50 -0500

    Add tests for missing fields for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,13 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    check_passwords 
     if user.save
       redirect_to "/users/#{user.id}"
       flash[:success] = "Welcome, #{user.name}!"
     else
       flash[:alert] = "Error: #{error_message(user.errors)}"
+      binding.pry 
       redirect_to '/register'
     end
   end
@@ -20,8 +22,13 @@ class UsersController < ApplicationController
   end
 
   private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
+    def check_passwords
+      if params[:password] != params[:password_conf]
+        
+      end
+    end
 end
