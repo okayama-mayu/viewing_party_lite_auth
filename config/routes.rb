905e41318101b974676289fa7e19a4be08c82135
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-06 15:30:19 -0500

    Add POST /login route to users#login

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[show create]
 
   get '/register', to: 'users#new'
-  
+
   get '/login', to: "users#login_form" 
+  post '/login', to: 'users#login' 
 
   get '/users/:id/discover', to: 'movies#discover'
   get '/users/:id/movies/:movie_id', to: 'movies#show'
