035a0095bb9d33b2262347b2b1e6da0014b2d62f
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-06 15:29:19 -0500

    Add login form view

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Logging In' do 
   # As a registered user
-    # When I visit the landing page `/`
-    # I see a link for "Log In"
-    # When I click on "Log In"
-    # I'm taken to a Log In page ('/login') 
-    # where I can input my unique email and password.
-    # When I enter my unique email and correct password 
-    # I'm taken to my dashboard page
-  end
+  # When I visit the landing page `/`
+  # I see a link for "Log In"
+  # When I click on "Log In"
+  # I'm taken to a Log In page ('/login') 
+  # where I can input my unique email and password.
+  # When I enter my unique email and correct password 
+  # I'm taken to my dashboard page
   it 'can log in with valid credentials' do 
     user = User.create!(name: 'Carlos Stich', email: 'c@gmail.com', password: 'abcdefg')
 
